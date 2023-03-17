de87efecdf37e7a1dd8f9cca90afbd9af59c6654
alienfoxx <alienfoxx001@gmail.com>
2023-03-17 13:08:43 -0700

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 if the program executed successfully
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+	putchar(n);
+	n--;
+	}
+	putchar('\n');
+	return (0);
+}
