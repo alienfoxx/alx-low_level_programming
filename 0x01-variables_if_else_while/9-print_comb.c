d74b61d0949815c7373df190f3af78a7dacfdb82
alienfoxx <alienfoxx001@gmail.com>
2023-03-17 14:03:33 -0700

    patience presistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point*
+ * Return: always 0 if the program executed successfully
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+	putchar(n);
+	putchar(',');
+	putchar(' ');
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
