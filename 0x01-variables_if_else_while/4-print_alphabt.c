4f4abfd699fdf8fd2c306f11eab5dd7aaa77ea51
alienfoxx <alienfoxx001@gmail.com>
2023-03-17 11:55:34 -0700

    alphapet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 if the program executed successfully
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
