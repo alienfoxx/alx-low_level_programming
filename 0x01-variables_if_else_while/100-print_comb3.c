40e97a92ef78dc61280c51eaa1b0c58461c926f8
alienfoxx <alienfoxx001@gmail.com>
2023-03-18 06:02:26 -0700

    Inventing is combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,20 @@ int main(void)
 	int n;
 	int m;
 
-	for (n = 0; n <= 8; n++)
+	for (n = 0; n < 9; n++)
 	{
-	for (m = n + 1 ; m <= 9; m++)
-	{
-		if (n < 9 || m < 9)
+		for (m = n + 1; m < 10; m++)
 		{
+			putchar((n % 10) + '0');
+			putchar((m % 10) + '0');
+
+			if (n == 8 && m == 9)
+				continue;
 			putchar(',');
 			putchar(' ');
+
 		}
-		putchar(n + 48);
-		putchar(m + 48);
-	}
+
 	}
 	putchar('\n');
 	return (0);
